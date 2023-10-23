07b94ceb75cd3a48b9d33096cfc4757390bf2132
Laksri1 <egc.laksri@gmail.com>
2023-10-23 06:34:31 +0530

    Setup1

---
+++ build/waling_algorithm/CMakeFiles/waling_algorithm_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/waling_algorithm_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/waling_algorithm_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
